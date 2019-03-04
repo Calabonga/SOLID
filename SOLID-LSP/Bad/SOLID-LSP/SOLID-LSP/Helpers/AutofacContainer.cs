1b7040d02bc6339db7f37628b9f99cd69af9ff27
Calabonga <pukhovnikov@inexika.com>
2019-03-04 11:48:23 +0700

    New principle demo template added

---
+++ SOLID-LSP/Bad/SOLID-LSP/SOLID-LSP/Helpers/AutofacContainer.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+
+namespace SOLID_OCP.Demo.Helpers
+{
+    /// <summary>
+    /// Autofac container initializer
+    /// </summary>
+    public static class AutofacContainer
+    {
+        /// <summary>
+        /// Returns an instance of the <see cref="IContainer"/>
+        /// </summary>
+        /// <returns></returns>
+        public static IContainer Create()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<AppSettingsManager>().AsSelf();
+            return builder.Build();
+        }
+    }
+}
---
+++ SOLID-LSP/Bad/SOLID-LSP/SOLID-LSP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOLID_OCP.Demo.Helpers;
+
+namespace SOLID_LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Container creations and some resolvings
+            var container = AutofacContainer.Create();
+            var settingsManager = container.Resolve<AppSettingsManager>();
+            #endregion
+        }
+    }
+}
